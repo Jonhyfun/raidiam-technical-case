7bc0563e935d64770cd846b12886c6ae28d621bb
João Paulo Grimaldi <jp.grimaldi@nv99.com.br>
2024-06-15 19:46:10 -0300

    feat: Displaying ApexCharts Pie Chart + New navbar header.

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data: function () {
+    return {
+      series: [44, 55, 13, 43, 22],
+      chartOptions: {
+        chart: {
+          width: 380,
+          type: 'pie'
+        },
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+})
+</script>
+
+<template>
+  <apexchart type="pie" width="500" :options="chartOptions" :series="series"></apexchart>
+</template>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="fixed flex gap-10 top-0 left-0 right-0 items-center h-11 bg-emerald-900 z-30">
+    <div class="w-32 flex justify-center">
+      <slot name="logo"></slot>
+    </div>
+    <nav class="flex gap-3 w-full">
+      <ul>
+        <slot name="links"></slot>
+      </ul>
+    </nav>
+  </header>
+</template>
